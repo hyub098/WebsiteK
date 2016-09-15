779f516f4396a2e417b19c5038da7dbf6a1960a2
Peter <hyub098@aucklanduni.ac.nz>
2016-09-15 17:46:33 +1200

    desc and date values on submit

--- imports/ui/timeline/NewTimeLine.jsx
+++ imports/ui/timeline/NewTimeLine.jsx
@@ -8,28 +8,38 @@ export default class NewTimeline extends React.Component {
 	componentDidMount(){
 
 		$('#datetimepicker1').datepicker();
-
-		// $('#datetimepicker1').datetimepicker();
-
 	}
 
+	onSubmit(e){
+		e.preventDefault();
+		var desc = document.getElementById("Textarea").value;
+		var date = document.getElementById("date").value;
+		if(desc.trim().length <= 1 || date.trim().length <= 1){
+			alert("Fill in details!");
+			return;
+		}
+		alert(desc + date);
+	}
 
    render() {
 
       return (
       	<div>
-      		<br />
-      		<br />
-      		<br />
-      		<br />
-      		<br />
-      		<br />
-      		<br />
-      		<br />
-      		<br />
-      		<br />
+      		<section className="global-page-header">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="block">
+                                <h2>Create new Achievements</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <br />
+            <br />
 	       	<section className="container">
-	       	<form>
+	       	<form onSubmit={this.onSubmit}>
 	      		<div className="fileinput fileinput-new" data-provides="fileinput">
 				  <div className="fileinput-new thumbnail" style={{width: 200, height: 150}}>
 				    <img data-src="holder.js/100%x100%" alt="..." />
@@ -40,12 +50,16 @@ export default class NewTimeline extends React.Component {
 				    <a href="#" className="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
 				  </div>
 				</div>
+				<div className="form-group">
+				    <label for="Textarea">Description</label>
+				    <textarea className="form-control" id="Textarea" rows="3"></textarea>
+				  </div>
 
 				<div className="row">
-			        <div className='col-sm-6'>
+			        <div className='col-sm-3'>
 			            <div className="form-group">
 			                <div className='input-group date' id='datetimepicker1'>
-			                    <input type='text' className="form-control" />
+			                    <input type='text' className="form-control" id="date"/>
 			                    <span className="input-group-addon">
 			                        <span className="glyphicon glyphicon-calendar"></span>
 			                    </span>
@@ -62,6 +76,10 @@ export default class NewTimeline extends React.Component {
 			
 
 	    	</section>
+
+	    	<br />
+	    	<br />
+	    	<br />
     	</div>
       );
    }
