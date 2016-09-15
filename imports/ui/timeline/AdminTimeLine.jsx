63334f2dca86c04c0d25c29ca6d68ab6dbf0cd4b
Peter <hyub098@aucklanduni.ac.nz>
2016-09-15 12:17:24 +1200

    close and edit style

--- imports/ui/timeline/AdminTimeLine.jsx
+++ imports/ui/timeline/AdminTimeLine.jsx
@@ -9,9 +9,91 @@ export default class AdminTimeLine extends React.Component {
    render() {
       return (
        	<div>
-              AdminTimeLine   
+            <div className="container">
+                <div className="page-header text-center">
+                    <h1 id="timeline">Timeline</h1>
+                    <div className="row">
+                        <button type="button" className="btn btn-default newButton">New</button>
+                    </div>
+                </div>
+                
+                <ul className="timeline">
+                    
+                    <li  className="timeline-inverted">
 
-       	</div>
+                      <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record invert" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
+                      <div className="timeline-panel">
+
+                        <div className="timeline-heading">
+
+                          <img className="img-responsive" src="pikachu.jpg"  style={{"width":"100%","height":500}}/> 
+                          <a className="boxclose" id="boxclose" ></a>
+                        <a className="boxedit" id="boxedit" ></a>
+
+                        </div>
+                        <div className="timeline-body">
+                          <p>皮卡丘的出現啟發了我。I gotta catch'em all! </p>
+                          <p className="timeline-date">2016-09-15</p>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
+                      <div className="timeline-panel">
+                        <div className="timeline-heading">
+                          <img className="img-responsive" src="question.jpg"  style={{"width":"100%","height":500}} />            
+                        </div>
+                        <div className="timeline-body">
+                          <p>I started to question my life, I can't continue this way.</p>
+                        </div>         
+                      </div>
+                    </li>
+                    
+                    <li  className="timeline-inverted">
+                      <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record invert" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
+                      <div className="timeline-panel">
+                        <div className="timeline-heading">
+                          <img className="img-responsive" src="snake.jpg"  style={{"width":"100%","height":500}}/>
+                          
+                        </div>
+                        <div className="timeline-body">
+                          <p>And I became so lazy.</p>
+                          
+                        </div>
+                        
+                      </div>
+                    </li>
+                    <li>
+                      <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
+                      <div className="timeline-panel">
+                        <div className="timeline-heading">
+                          <img className="img-responsive" src="home.jpg"  style={{"width":"100%","height":500}} />
+                          
+                        </div>
+                        <div className="timeline-body">
+                          <p>Then, I start living happily in their house.</p>
+                          
+                        </div>
+                        
+                      </div>
+                    </li>
+                    
+                    <li  className="timeline-inverted">
+                      <div className="timeline-badge primary"><a><i className="glyphicon glyphicon-record invert" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
+                      <div className="timeline-panel">
+                        <div className="timeline-heading">
+                          <img className="img-responsive" src="caught.jpg"  style={{"width":"100%","height":500}}/>
+                          
+                        </div>
+                        <div className="timeline-body">
+                          <p>I was caught by brothers of three men.</p>
+                          
+                        </div>
+                      </div>
+                    </li>      
+                </ul>
+            </div>
+        </div>
       );
    }
 }
